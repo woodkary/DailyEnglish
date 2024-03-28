ae153c6b3329044054ba022b9bcaf493ff711ebc
Zigen <czg2012@qq.com>
2024-03-28 14:22:15 +0800

    service

---
+++ Service/Random.go
@@ -0,0 +1,13 @@
+package service
+
+import "math/rand"
+
+//生成随机字符串, n为字符串长度
+func RandomString(n int) string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	result := make([]byte, n)
+	for i := range result {
+		result[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(result)
+}
