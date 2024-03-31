b2bda23f19896ebf53f74835b469a4916e579fb9
karywood <834479572@qq.com>
2024-03-31 18:34:32 +0800

    页面模板

--- static/js/Navbar.js
+++ static/js/Navbar.js
@@ -2,3 +2,20 @@ function logout(){
     sessionStorage.removeItem("token");
     window.location.href = 'login.html';
 }
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// 点击下拉菜单以外的地方时隐藏下拉内容
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
