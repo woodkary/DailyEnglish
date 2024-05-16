f9c3fef90c2c080e6c92d854fb72785f2c6b5ef5
MRcyf2 <496357312@qq.com>
2024-05-16 19:12:20 +0800

    简单写了个搜索框

--- app/DailyEnglish/pages/home/home.vue
+++ app/DailyEnglish/pages/home/home.vue
@@ -1,9 +1,12 @@
 <template>
 	<view class="homepage">
-		<view class="search-container" >
-			<view class="search" @click="handleSearch">
-				<image class="search-icon" src="/static/search.svg"></image>
-				<input placeholder="搜索" v-model:value="searchInput">
+		<view class="search-container">
+			<view class="search-head" style="display: flex;">
+				<view class="search" :class="{active:isHistoryVisible}" @click="handleSearch">
+					<image class="search-icon" src="/static/search.svg"></image>
+					<input placeholder="搜索" v-model:value="searchInput">
+				</view>
+				<button class="cancel" v-show="isHistoryVisible" @click="cancelSearch">取消</button>
 			</view>
 			<view class="history" v-show="isHistoryVisible">
 				<view class="history-header">
@@ -11,7 +14,8 @@
 					<text class="clean">清空</text>
 				</view>
 				<view class="list">
-					<view class="item" v-for="(item, index) in items" :key="index" @click="handleSearchInput(item.word)">
+					<view class="item" v-for="(item, index) in items" :key="index"
+						@click="handleSearchInput(item.word)">
 						<view class="top-row">
 							<view class="word">{{ item.word }}</view>
 							<view class="phonetic">{{ item.phonetic }}</view>
@@ -41,11 +45,22 @@
 		background-color: #fff;
 		border-radius: 50rpx;
 		display: flex;
-		height: 60rpx;
+		height: 65rpx;
 		padding: 5rpx;
 		width: 70%;
 		align-items: center;
+		margin-left: 45rpx;
+		/* transition: all 0.3s ease; */
+		/* 为所有属性添加过渡效果 */
+	}
+
+	.search.active {
+		align-items: flex-start;
+		/* 从 left 改为 flex-start */
+		border-radius: 15rpx;
+		width: 75%;
 		margin-left: 40rpx;
+		padding: 5rpx;
 	}
 
 	.search-icon {
@@ -53,9 +68,19 @@
 		height: 25px;
 		margin-left: 200rpx;
 		margin-right: 20rpx;
+		/* transition: all 0.3s ease; */
+		/* 为所有属性添加过渡效果 */
 	}
 
-	.search input {
+	.search.active .search-icon {
+		margin-left: 20rpx;
+		margin-right: 0;
+		width: 25px;
+		height: 25px;
+		margin-top: 10rpx;
+	}
+
+	input {
 		flex: 1;
 		border: none;
 		outline: none;
@@ -63,6 +88,33 @@
 		width: 10rpx;
 		font-size: 30rpx;
 		max-width: 60rpx;
+		/* transition: all 0.3s ease; */
+		/* 为所有属性添加过渡效果 */
+	}
+
+	.cancel {
+		font-size: 40rpx;
+		margin-top: -20rpx;
+		font-weight: 530;
+		background-color: transparent;
+		color: #000000;
+		border: none;
+
+		&::after {
+			border: none;
+		}
+
+	}
+
+
+	.search.active input {
+		width: 80%;
+		text-align: left;
+		max-width: 80%;
+		height: 100%;
+		font-size: 38rpx;
+		margin-left: 10rpx;
+		/* margin-top:10rpx; */
 	}
 
 	.history {
@@ -70,18 +122,12 @@
 		width: 100%;
 		background-color: #fff;
 		height: calc(100vh - 60rpx);
-		/* display:none; */
 	}
-
 
 	.history-header {
 		display: flex;
 		justify-content: space-between;
-		/* 使.title和.clean分别位于顶部两边 */
 		align-items: center;
-		/* padding-top: 20rpx;
-		padding-left: 30rpx;
-		padding-right: 30rpx; */
 		padding: 30rpx;
 	}
 
@@ -91,8 +137,9 @@
 	}
 
 	.clean {
-		font-size: 30rpx;
+		font-size: 35rpx;
 		color: #767676;
+		cursor: pointer;
 	}
 
 	.list {
@@ -106,7 +153,7 @@
 	.item {
 		margin-bottom: 10px;
 		border-bottom: 1px solid #cecece;
-		height:130rpx;
+		height: 130rpx;
 	}
 
 	.top-row {
@@ -140,29 +187,32 @@
 		data() {
 			return {
 				isHistoryVisible: false,
-        searchInput: '',
+				searchInput: '',
 				items: [{
 						word: 'apple',
 						phonetic: '/ˈæpl/',
 						meaning: '苹果111111111111111111111111111111111111111111111111'
-					},{
+					}, {
 						word: 'banana',
 						phonetic: '/bəˈnɑː.nə/',
 						meaning: '香蕉'
 					}
-					
+
 					// 其他列表项
 				]
 			}
 		},
 		methods: {
 			handleSearch() {
-				this.isHistoryVisible = !this.isHistoryVisible;
+				this.isHistoryVisible = true;
 			},
-      handleSearchInput(input) {
-        this.searchInput = input;
-      }
+			handleSearchInput(input) {
+				this.searchInput = input;
+			},
+			cancelSearch() {
+				this.isHistoryVisible = false;
+				this.searchInput = '';
+			}
 		}
 	}
-	
 </script>
