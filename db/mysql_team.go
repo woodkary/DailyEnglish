117cc433c4a7256579bd35dbcc03b21a7027a4bb
Carl <2906198562@qq.com>
2024-05-28 20:48:51 +0800

    sql.team

---
+++ db/mysql_team.go
@@ -0,0 +1,69 @@
+package db
+
+import (
+	utils "DailyEnglish/utils"
+	"database/sql"
+	"time"
+)
+
+// （新建团队）插入团队信息
+func RegisterTeam(db *sql.DB, teamname string, managerid, maxnum int) error {
+	// 准备插入语句
+	teamid := utils.GenerateID(time.Now(), 1)
+	// 获取当前日期
+	now := time.Now()
+	// 格式化日期为字符串
+	today := now.Format("2006-01-02")
+
+	stmt, err := db.Prepare("INSERT INTO team_info(team_id,manager_id,team_name, member_num,build_date) VALUES( ?, ?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	// 执行插入语句
+	_, err = stmt.Exec(teamid, managerid, teamname, maxnum, today)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//删除团队
+//DELETE FROM employees WHERE employee_id = 123;
+
+func DeleteTeam(db *sql.DB, teamid int) error {
+
+	stmt, err := db.Prepare("DELETE FROM team_info WHERE team_id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	// 执行插入语句
+	_, err = stmt.Exec(teamid)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 删除团队成员
+func DeleteTeammember(db *sql.DB, teamid int, username string) error {
+
+	userid, err := GetUserID(db, username)
+
+	stmt, err := db.Prepare("DELETE FROM user-team WHERE user_id = ? AND team_id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(userid, teamid)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
