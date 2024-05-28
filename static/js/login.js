d1d2549f835b6a2d8c81987337b85562a63b884b
karywood <834479572@qq.com>
2024-05-28 18:11:51 +0800

    8081端口加入跨域

--- static/js/login.js
+++ static/js/login.js
@@ -102,31 +102,8 @@ function login(event) {
             localStorage.setItem("team_info", data.team_info);
             let token = data.token;
             console.log(token);
-           /* let url = 'http://localhost:8081/api/team_manage/personal_center/data';*/
-/*            fetch(url, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + token
-                }
-            }).then(response => {
-                console.log(response);
-                return response.json();
-            }).then(data => {
-                console.log(data);
-                if (data.code == 200) {
-                    console.log(data.data);
-                    localStorage.setItem("name", data.user.name);
-                    localStorage.setItem("email", data.user.email);
-                    localStorage.setItem("team", data.user.team);
-                    localStorage.setItem("right", data.user.right);
-                } else {
-                    console.log(data.message);
-                }
-            }).catch(error => {
-                console.log(error);
-            })*/
-            /*window.location.href = 'http://localhost:8081/api/team_manager/index';*/
+            
+            //跳转到主页
             window.location.href = 'user.html';//跳转到主页,为了能展示，先暂存
         } else {
             var message = document.getElementById("verification-message");
