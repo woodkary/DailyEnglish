cc0077cb9e5ac84b204aaa24ab3cc0b08e9ad333
MRcyf2 <496357312@qq.com>
2024-06-05 15:24:39 +0800

    web的注册验证码发送中添加了redis的使用

--- go.mod
+++ go.mod
@@ -14,6 +14,11 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/PuerkitoBio/goquery v1.9.1 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
@@ -26,6 +31,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
 	github.com/howeyc/fsnotify v0.9.0 // indirect
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	userrouter "DailyEnglish/router/user_router"
 	"database/sql"
 	"fmt"
-
+	"github.com/go-redis/redis/v8"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -20,6 +20,21 @@ func main() {
 	hostname := "rm-wz9p61j3qlj6lg69fpo.mysql.rds.aliyuncs.com"
 	port := "3306"
 	dbname := "dailyenglish"
+
+	// redis 连接信息
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "r-bp1jdmrszl1yd6xxdipd.redis.rds.aliyuncs.com:6379", // Redis服务器地址
+		Username: "mimahezhanghao1yang",                            // Redis账号
+		Password: "MIMAhezhanghao1yang",                              // Redis密码
+		DB:       0,                                                 // 选择的数据库
+	})
+ 
+	// 测试Redis连接
+	_, err := rdb.Ping(rdb.Context()).Result()
+	if err != nil {
+		panic(err)
+	}
+	defer rdb.Close()
 
 	// 构建数据库连接字符串
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, hostname, port, dbname)
@@ -40,7 +55,7 @@ func main() {
 	teamrouter.InitTeamRouter(r, db)
 	go func() {
 		r1 := gin.Default()
-		userrouter.InitUserRouter(r1, db)
+		userrouter.InitUserRouter(r1, db,rdb)
 		r1.Use(middlewares.Cors())
 		r1.Run(":8080")
 	}()
